5b157cd8c323ae1d3b57428230e870e9493df4f7
virus01064 <jp1jn04@gmail.com>
2024-04-26 21:40:15 +0800

    upadate landing page (add JS on navbar dan button and textarea) and page 2 (add responsive on description)

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const navbarNav = document.querySelector(".navbarlink");
-//sesudah
 document.querySelector("#hamburger").onclick = () => {
   navbarNav.classList.toggle("active");
 };
@@ -9,3 +8,41 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+// comment
+const komen = document.querySelector("textarea");
+komen.addEventListener("keyup", (e) => {
+  komen.style.height = "auto";
+  let tinggi = e.target.scrollHeight;
+  komen.style.height = `${tinggi}px`;
+});
+
+function acc() {
+  alert("FITUR BELUM TERSEDIA!");
+}
+setTimeout(function send() {
+  alert("PESAN TERKIRIM!");
+},2000);
+
+// navbar scroll
+const nav = document.querySelector(".navbarnav");
+const baga = document.querySelector("header");
+const d = document.querySelector(".drop");
+
+const bagaOptions = {
+  rootMargin: "-200px 0px 0px 0px",
+};
+
+const bagaObserver = new IntersectionObserver(function (entries, bagaObserver) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      nav.classList.add("aktif");
+      d.classList.add("aktif");
+    } else {
+      d.classList.remove("aktif");
+      nav.classList.remove("aktif");
+    }
+  });
+}, bagaOptions);
+
+bagaObserver.observe(baga);
